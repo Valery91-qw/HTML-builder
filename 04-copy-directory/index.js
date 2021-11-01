c60606bc06deaa85d43de2b9263f9195dc3a519a
Valery91-qw <vakriv91@gmail.com>
2021-11-01 19:20:37 +0300

    feat: complete 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const pathToDirectory = path.join(__dirname, 'files');
 
+function copyFiles(currentDirectory) {
+  fs.mkdir(currentDirectory + '-copy', {recursive: true},(err) => {
+    if(err) throw err;
+  });
+  fs.readdir(currentDirectory, {withFileTypes : true}, (err, files) => {
+
+    if(err) throw err;
+
+    for (let file of files){
+
+      if(file.isFile()){
+        fs.createReadStream(`${currentDirectory}/${file.name}`)
+          .pipe(fs.createWriteStream(`${currentDirectory}-copy/${file.name}`));
+      }
+    }
+  });
+}
+
+copyFiles(pathToDirectory);
