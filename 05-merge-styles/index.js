f4ec26d2fbda091983f2479e54c55ad8acc685c5
Valery91-qw <vakriv91@gmail.com>
2021-11-05 18:52:35 +0300

    feat: complete task 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,18 +1,34 @@
 const fs = require('fs');
 const path = require('path');
 const pathToDirectory = path.join(__dirname, 'styles');
+const pathToBundle = path.join(__dirname, 'project-dist', 'bundle.css');
 
 function createBundle(currentPath) {
+
+  fs.access(pathToBundle,() => {
+    fs.rm(pathToBundle, () => {
+    });
+  });
+
   fs.readdir(currentPath, {withFileTypes: true}, (err, files) => {
     if(err) throw err;
-        
+
     const allCorrectFiles = [];
-    
+
     for (let file of files) {
-      if(file.name.match(/.css/)) allCorrectFiles.push(file.name);
+      if(file.name.match(/.css$/)) {
+        allCorrectFiles.push(file.name);
+      }
     }
 
-    console.log(allCorrectFiles);
+    for (let file of allCorrectFiles) {
+      fs.createReadStream(`${currentPath}/${file}`, 'utf8')
+        .on('data', chank => {
+          fs.appendFile(pathToBundle, chank, (err) => {
+            if(err) throw err;
+          });
+        });
+    }
   });
 }
 
