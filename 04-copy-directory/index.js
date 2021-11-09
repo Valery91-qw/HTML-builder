eb02fca1625a07b3c50bd988f77635ea59c49920
Valery91-qw <vakriv91@gmail.com>
2021-11-09 16:26:53 +0300

    feat: fix task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,6 +1,51 @@
+const fs = require('fs');
 const path = require('path');
-const {copyFiles} = require('./copyFiles');
 const pathToDirectory = path.join(__dirname, 'files');
 const pathToCopy = path.join(__dirname, 'files-copy');
+
+
+function copyFiles(fromDirectory, toDirectory) {
+  fs.access(toDirectory,  (err) => {
+    if(!err) {
+      fs.readdir(toDirectory, (err, files) => {
+
+        for (let file of files) {
+          fs.rm(`${toDirectory}/${file}`, () => {
+            fs.readdir(fromDirectory, {withFileTypes : true}, (err, files) => {
+
+              if(err) throw err;
+
+              for (let file of files){
+
+                if(file.isFile()){
+                  fs.createReadStream(`${fromDirectory}/${file.name}`)
+                    .pipe(fs.createWriteStream(`${toDirectory}/${file.name}`));
+                }
+              }
+            });
+          });
+        }
+      });
+    } else {
+      fs.mkdir(toDirectory, {recursive: true},(err) => {
+        if(err) throw err;
+
+        fs.readdir(fromDirectory, {withFileTypes : true}, (err, files) => {
+
+          if(err) throw err;
+
+          for (let file of files){
+
+            if(file.isFile()){
+              fs.createReadStream(`${fromDirectory}/${file.name}`)
+                .pipe(fs.createWriteStream(`${toDirectory}/${file.name}`));
+            }
+          }
+        });
+      });
+    }
+  });
+
+}
 
 copyFiles(pathToDirectory, pathToCopy);
